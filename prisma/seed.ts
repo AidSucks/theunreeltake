96184e2b901aab012219bee87facdfe877ac5de3
Aidan Anderson <aidananderson167@gmail.com>
2025-03-04 16:08:46 -0800

    Fix: Seeding Database Script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -15,3 +15,12 @@ export async function main() {
     await prisma.user.create({ data: u })
   }
 }
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
