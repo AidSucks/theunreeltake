d2e5baa9ad4fd0694aabc2a303a30d412957b23e
Aidan Anderson <aidananderson167@gmail.com>
2025-10-13 19:39:30 -0700

    v0.1.0; Migrate to eslint CLI

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,13 +5,13 @@ const compat = new FlatCompat({
   baseDirectory: import.meta.dirname,
 });
 
-const eslintConfig = [
-  ...compat.config({
-      extends: ['next/core-web-vitals', 'next/typescript'],
-      rules: {
-          '@typescript-eslint/no-unused-vars': 'off'
-      }
-  }),
-];
+const eslintConfig = [{
+  ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
+}, ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript'],
+    rules: {
+        '@typescript-eslint/no-unused-vars': 'off'
+    }
+})];
 
 export default eslintConfig;
