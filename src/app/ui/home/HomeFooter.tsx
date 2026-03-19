c4ea90ea324e2dfcfb43a917b75073e79848c98c
LoganJ <jenkinsloganb@gmail.com>
2026-03-19 11:27:56 -0700

    "Jira: URT-11" * As a visitor, I should be able to navigate to the home, about, requests and catalog pages using the footer section.

---
+++ src/app/ui/home/HomeFooter.tsx
@@ -0,0 +1,40 @@
+import {Box, Flex, Title} from '@mantine/core';
+import {SocialMediaLinks} from "@/app/ui/home/SocialMediaLinks";
+import {WebNavLinks} from "@/app/ui/home/WebNavLinks";
+import {Bootstrap, BorderTop} from "react-bootstrap-icons";
+
+export function HomeFooter() {
+
+  return (
+    <Flex h={"100%"} w={"100%"} p={"xs"} align={"flex-end"} direction={"column"} 
+    style={{'borderStyle': 'solid hidden hidden hidden', 'borderWidth': '1px'}}>
+
+      <Flex h={"50%"} w={"inherit"} visibleFrom={"sm"}>
+
+        <Box w={"25%"} h={"100%"}>
+          <Flex h={"100%"} w={"100%"} align={"center"} justify={"flex-start"} pl={{ base: "none", md: "md"}} columnGap={8}>
+            <Bootstrap size={30}/>
+            <Title order={4} textWrap={"nowrap"}>The UnReel Take</Title>
+          </Flex>
+        </Box>
+
+        <Box w={"50%"} h={"100%"}>
+          <Flex h={"100%"} w={"100%"} align={"center"} justify={"center"} px={"xs"}>
+            {/**Center Media**/}
+            <WebNavLinks/>
+          </Flex>
+        </Box>
+
+        {/**For Jalen: Social Media stuff goes here*/}
+        <Box w={"25%"} h={"100%"}>
+          <Flex h={"100%"} w={"100%"} align={"right"} justify={"center"} px={"xs"}>
+            {/**Right Media**/}
+            Placeholder
+          </Flex>
+        </Box>
+
+      </Flex>
+
+    </Flex>
+  );
+}
