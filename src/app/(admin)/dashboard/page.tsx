49414535c6ea2db3c91698ce4779aa1c50a21884
LoganJ <jenkinsloganb@gmail.com>
2026-03-20 22:09:30 -0700

    URT-38 * "As a user, I should be redirected to the dashboard page after signing in"

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -3,6 +3,7 @@
 import {useContext} from "react";
 import {AuthContext} from "@/app/ui/admin/AuthContext";
 import {redirect} from "next/navigation";
+import {Flex, Group} from '@mantine/core';
 
 export default function DashboardPage() {
 
@@ -12,10 +13,87 @@ export default function DashboardPage() {
 
   return (
     <>
-      <h1>Dashboard</h1>
-      <p>Name: {contextData.user.name}</p>
-      <p>Email: {contextData.user.email}</p>
-      <p>Role: {contextData.user.role}</p>
+      <h1>Welcome,  {contextData.user.name}!</h1>
+      
+      <Flex 
+      mih={500}
+      gap="xs"
+      justify="left"
+      align="left"
+      direction="column"
+      wrap="wrap"
+      >
+        <Flex 
+        mih={250} 
+        w={"100%"}
+        gap="xl"
+        direction="row"
+        wrap="wrap" >
+          <Group gap={"xl"} wrap={"nowrap"} justify={"center"} align = {"center"} w={"75%"}
+          style={{'borderStyle': 'solid', 'borderWidth': '3px', padding: '5px', borderRadius: "12px"}}>
+            Box 1
+          </Group>
+          <Group gap={"xl"} wrap={"nowrap"} justify={"center"} align = {"center"} w={"20%"}
+          style={{'borderStyle': 'solid', 'borderWidth': '3px', padding: '5px', borderRadius: "12px"}}>
+            Box 2
+          </Group>
+        </Flex>
+
+        <Flex 
+        mih={250} 
+        w={"100%"}
+        gap="xl"
+        direction="row"
+        wrap="wrap" >
+          <Group gap={"xl"} wrap={"nowrap"} justify={"center"} align = {"center"} w={"33%"}
+          style={{'borderStyle': 'solid', 'borderWidth': '3px', padding: '5px', borderRadius: "12px"}}>
+            Box 3
+          </Group>
+          <Group gap={"xl"} wrap={"nowrap"} justify={"center"} align = {"center"} w={"30%"}
+          style={{'borderStyle': 'solid', 'borderWidth': '3px', padding: '5px', borderRadius: "12px"}}>
+            Box 4
+          </Group>
+          <Group gap={"xl"} wrap={"nowrap"} justify={"center"} align = {"center"} w={"30%"}
+          style={{'borderStyle': 'solid', 'borderWidth': '3px', padding: '5px', borderRadius: "12px"}}>
+            Box 5
+          </Group>
+        </Flex>
+      </Flex>
+
+      <h2>Recent Reviews</h2>
+
+      <Flex 
+      mih={200}
+      gap="xs"
+      justify="left"
+      align="left"
+      direction="column"
+      wrap="wrap"
+      >
+        <Flex 
+        mih={190} 
+        w={"100%"}
+        gap="xl"
+        direction="row"
+        wrap="wrap" >
+          <Group gap={"xl"} wrap={"nowrap"} justify={"center"} align = {"center"} w={"23%"}
+          style={{'borderStyle': 'solid', 'borderWidth': '3px', padding: '5px', borderRadius: "12px"}}>
+            Review 1
+          </Group>
+          <Group gap={"xl"} wrap={"nowrap"} justify={"center"} align = {"center"} w={"23%"}
+          style={{'borderStyle': 'solid', 'borderWidth': '3px', padding: '5px', borderRadius: "12px"}}>
+            Review 2
+          </Group>
+          <Group gap={"xl"} wrap={"nowrap"} justify={"center"} align = {"center"} w={"23%"}
+          style={{'borderStyle': 'solid', 'borderWidth': '3px', padding: '5px', borderRadius: "12px"}}>
+            Review 3
+          </Group>
+          <Group gap={"xl"} wrap={"nowrap"} justify={"center"} align = {"center"} w={"23%"}
+          style={{'borderStyle': 'solid', 'borderWidth': '3px', padding: '5px', borderRadius: "12px"}}>
+            Review 4
+          </Group>
+        </Flex>
+      </Flex>
     </>
   );
 
