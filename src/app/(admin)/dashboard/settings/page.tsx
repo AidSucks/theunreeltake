1d903115eeb633e7693439501c4b38a5ec55c5a9
Izak Puente <puenteizak@live.com>
2026-03-20 21:41:46 -0700

    URT 209 & URT 212

--- src/app/(admin)/dashboard/settings/page.tsx
+++ src/app/(admin)/dashboard/settings/page.tsx
@@ -2,7 +2,7 @@ import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import { ChangePassword } from "@/app/ui/admin/forms/ChangePassword";
-import { Box, Title, Divider } from "@mantine/core";
+import { Box, Title, Divider,Text,TextInput } from "@mantine/core";
 import { PasswordSettings } from "./PasswordSettings";
 
 export default async function DashboardSettingsPage() {
@@ -16,8 +16,11 @@ export default async function DashboardSettingsPage() {
     <Box>
       <Title order={2} mb="md">Account Settings</Title>
       <Divider mb="xl" />
-      
+      <Title order={6} mb="xs">Email Address</Title>
+      <Text mb="lg">testuser@domain.com</Text>
       <PasswordSettings/>
+      <Title order={6} mb="xs">Personal Information</Title>
+      <TextInput pl="lg" w={200} size="sm" radius="lg" label="Username" placeholder="New Username"/>
     </Box>
   );
 }
