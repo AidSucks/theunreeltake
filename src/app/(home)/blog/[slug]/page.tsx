6445eaf357876d975dcc8ea52be6d108f3f88158
Izak Puente <puenteizak@live.com>
2026-04-07 16:19:17 -0700

    URT-235

--- src/app/(home)/blog/[slug]/page.tsx
+++ src/app/(home)/blog/[slug]/page.tsx
@@ -1,5 +1,6 @@
 
 import films from "@/../public/film.json";
+import { Title, Grid, GridCol,Image,Text } from "@mantine/core";
 
 export default async function BlogPostPage(
   {
@@ -16,7 +17,32 @@ export default async function BlogPostPage(
   if(!fullPostData) return <h1>Not Found</h1>;
 
   return (
+
+
     <div>
+      <Title ta="center" order={1}>{fullPostData["Title"]}</Title>
+      <Text ta= "center" size="sm">{fullPostData["Released"]} • {fullPostData["Rated"]} • {fullPostData["Runtime"]} • {fullPostData["Genre"]} • {fullPostData["Director"]} • {fullPostData["Country"]} • {fullPostData["Language"]}</Text>
+
+      <Grid justify="center" align="flex-start">
+      <GridCol span={2}><Image
+      radius="md"
+      
+      src={fullPostData.Images[0]}
+      />
+      </GridCol>
+      <GridCol span={2}><Image
+      radius="md"
+      
+      src={fullPostData.Images[1]}
+      />
+      </GridCol>
+      <GridCol span={2}><Image
+      radius="md"
+      
+      src={fullPostData.Images[2]}
+      />
+      </GridCol>
+      </Grid>
       {Object.entries(fullPostData).map((value, index) => {
         return <ul key={index}>{value[0].toString()} : {value[1].toString()}</ul>
       })}
