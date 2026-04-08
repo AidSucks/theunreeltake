eb8c8902cbe7790ee3ff146ad1baf8a9b22ef40b
Melvin <ly_melvin@yahoo.com>
2026-04-08 15:35:27 -0700

    URT-224 - URT-255 Create /posts/routt - URT-256 Create grid layout for post cards - URT-257 Create post card component - URT-258 Add "new" button for post creation - URT-259 Add pagination selector under grid - URT-260 Add search bar at top of grid - URT-261 Query database for all posts for certain user

---
+++ src/app/(admin)/dashboard/posts/page.tsx
@@ -0,0 +1,157 @@
+"use client"
+
+import { Button, Flex, TextInput, ActionIcon, Pagination, Grid, Group} from "@mantine/core";
+import { Search, Funnel, Filter, ArrowClockwise, PencilSquare, Chat, Trash, BarChart } from "react-bootstrap-icons"
+import { GridPostCard } from "./gridPost";
+
+export default function DashboardPostsPage() {
+    return(
+        <div style={{ padding: "0 40px" }}>
+            <h1>Your Posts</h1>
+                <Flex
+                    justify={"space-between"}
+                    gap={"md"}>
+                    <Group>
+                    <TextInput 
+                        w={400} 
+                        size={"xs"}  
+                        radius={"md"} 
+                        placeholder={"Search"}
+                        rightSection={<Search size={16}/>}
+                    ></TextInput>
+                    <ActionIcon 
+                        variant={"light"}
+                        color={"gray"}
+                        radius={"lg"}
+                        aria-label={"Filter Button"}>
+                        <Funnel size={16}/>
+                    </ActionIcon>
+                    <ActionIcon
+                        variant={"light"}
+                        color={"gray"}
+                        radius={"lg"}
+                        aria-label={"Sort Button"}>
+                        <Filter size={16}/>
+                    </ActionIcon>
+                    <ActionIcon
+                        variant={"light"}
+                        color={"gray"}
+                        radius={"lg"}
+                        aria-label={"Refresh Button"}>
+                        <ArrowClockwise size={16}/>
+                    </ActionIcon>
+                    </Group>
+                    <Group
+                        justify={"flex-end"}>
+                        <Button
+                            variant={"light"}
+                            size={"xs"}
+                            radius={"md"}
+                            color={"gray"}
+                            >
+                            + New
+                        </Button>
+                    </Group>
+                </Flex>
+
+                <Flex
+                    style={{ marginTop: '32px' }}>
+                    <Grid  
+                        gutter={"sm"}
+                        >
+                        <Grid.Col
+                            span={{ base: 12, md: 6, lg: 3 }}
+                            >
+                            <GridPostCard 
+                                imageSrc="https://placehold.co/600x800?text=Placeholder"
+                                icon_1={PencilSquare} 
+                                icon_2={Chat}
+                                icon_3={BarChart}
+                                icon_4={Trash} />
+                        </Grid.Col>
+                        <Grid.Col
+                            span={{ base: 12, md: 6, lg: 3 }}
+                            >
+                            <GridPostCard 
+                                imageSrc="https://placehold.co/600x800?text=Placeholder"
+                                icon_1={PencilSquare} 
+                                icon_2={Chat}
+                                icon_3={BarChart}
+                                icon_4={Trash} />
+                        </Grid.Col>
+                        <Grid.Col
+                            span={{ base: 12, md: 6, lg: 3 }}
+                            >
+                            <GridPostCard 
+                                imageSrc="https://placehold.co/600x800?text=Placeholder"
+                                icon_1={PencilSquare} 
+                                icon_2={Chat}
+                                icon_3={BarChart}
+                                icon_4={Trash} />
+                        </Grid.Col>
+                        <Grid.Col
+                            span={{ base: 12, md: 6, lg: 3 }}
+                            >
+                            <GridPostCard 
+                                imageSrc="https://placehold.co/600x800?text=Placeholder"
+                                icon_1={PencilSquare} 
+                                icon_2={Chat}
+                                icon_3={BarChart}
+                                icon_4={Trash} />
+                        </Grid.Col>
+                        <Grid.Col
+                            span={{ base: 12, md: 6, lg: 3 }}
+                            >
+                            <GridPostCard 
+                                imageSrc="https://placehold.co/600x800?text=Placeholder"
+                                icon_1={PencilSquare} 
+                                icon_2={Chat}
+                                icon_3={BarChart}
+                                icon_4={Trash} />
+                        </Grid.Col>
+                        <Grid.Col
+                            span={{ base: 12, md: 6, lg: 3 }}
+                            >
+                            <GridPostCard 
+                                imageSrc="https://placehold.co/600x800?text=Placeholder"
+                                icon_1={PencilSquare} 
+                                icon_2={Chat}
+                                icon_3={BarChart}
+                                icon_4={Trash} />
+                        </Grid.Col>
+                        <Grid.Col
+                            span={{ base: 12, md: 6, lg: 3 }}
+                            >
+                            <GridPostCard 
+                                imageSrc="https://placehold.co/600x800?text=Placeholder"
+                                icon_1={PencilSquare} 
+                                icon_2={Chat}
+                                icon_3={BarChart}
+                                icon_4={Trash} />
+                        </Grid.Col>
+                        <Grid.Col
+                            span={{ base: 12, md: 6, lg: 3 }}
+                            >
+                            <GridPostCard 
+                                imageSrc="https://placehold.co/600x800?text=Placeholder"
+                                icon_1={PencilSquare} 
+                                icon_2={Chat}
+                                icon_3={BarChart}
+                                icon_4={Trash} />
+                        </Grid.Col>
+                    </Grid>
+                    
+                </Flex>
+                    <Pagination 
+                        total={15} 
+                        siblings={1} 
+                        defaultValue={1} 
+                        color={"gray"}
+                        style={{
+                            marginTop: 32,
+                            bottom: 20,
+                            left: 300
+                        }}/>
+        </div>
+    );
+}
